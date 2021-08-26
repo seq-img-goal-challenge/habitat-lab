f44d1ee2636f7593491cd4833a361b3c90b40416
Guillaume Bono <guillaume.bono@insa-lyon.fr>
2021-08-26 09:53:22 +0200

    Minor refactor of local_rpc_eval

--- habitat/utils/local_rpc_evaluation.py
+++ habitat/utils/local_rpc_evaluation.py
@@ -1,8 +1,8 @@
 from typing import Optional, Dict, Union, Any
 import io
 import threading
-from xmlrpc.server import SimpleXMLRPCServer
-from xmlrpc.client import ServerProxy, Binary
+import xmlrpc.server
+import xmlrpc.client
 
 import numpy as np
 import torch
@@ -12,7 +12,7 @@ from habitat.core.agent import Agent
 from habitat.core.benchmark import Benchmark
 
 
-RPC_SERVER_PORT = 8000
+DEFAULT_RPC_PORT = 8000
 
 
 def _serialize_visual_observations(observations: Observations) -> bytes:
@@ -25,32 +25,12 @@ def _serialize_visual_observations(observations: Observations) -> bytes:
 
 def _deserialize_visual_observations(buf: bytes) -> Dict[str, np.ndarray]:
     buf = io.BytesIO(buf)
-    return np.load(buf)
-
-
-def run_rpc_agent(agent: Agent) -> None:
-    with SimpleXMLRPCServer(("localhost", RPC_SERVER_PORT),
-                            logRequests=False, allow_none=True) as rpc_server:
-        @rpc_server.register_function
-        def reset() -> None:
-            agent.reset()
-
-        @rpc_server.register_function
-        def act(serialized_obs: Binary) -> Union[int, str, Dict[str, Any]]:
-            obs = _deserialize_visual_observations(serialized_obs.data)
-            return agent.act(obs)
-
-        @rpc_server.register_function
-        def terminate() -> None:
-            t = threading.Thread(target=rpc_server.shutdown)
-            t.start()
-
-        rpc_server.serve_forever()
+    return {uuid: obs for uuid, obs in np.load(buf).items()}
 
 
 class AgentProxy(Agent):
     def __init__(self, agent_url) -> None:
-        self.proxy = ServerProxy(agent_url)
+        self.proxy = xmlrpc.client.ServerProxy(agent_url)
 
     def reset(self):
         self.proxy.reset()
@@ -63,13 +43,31 @@ class AgentProxy(Agent):
         self.proxy.terminate()
 
 
+def run_rpc_agent(agent: Agent, port=DEFAULT_RPC_PORT) -> None:
+    with xmlrpc.server.SimpleXMLRPCServer(("localhost", port),
+                                          logRequests=False, allow_none=True) as rpc_server:
+        @rpc_server.register_function
+        def reset() -> None:
+            agent.reset()
+
+        @rpc_server.register_function
+        def act(serialized_obs: xmlrpc.client.Binary) -> Union[int, str, Dict[str, Any]]:
+            obs = _deserialize_visual_observations(serialized_obs.data)
+            return agent.act(obs)
+
+        @rpc_server.register_function
+        def terminate() -> None:
+            t = threading.Thread(target=rpc_server.shutdown)
+            t.start()
+
+        rpc_server.serve_forever()
+
+
 def run_rpc_benchmark(config_paths: Optional[str]=None,
-                      agent_url: Optional[str]=None,
-                      num_episodes: Optional[int]=None) -> Dict[str, float]:
-    benchmark = Benchmark(config_paths, False)
-    if agent_url is None:
-        agent_url = f"http://localhost:{RPC_SERVER_PORT}"
-    agent = AgentProxy(agent_url)
+                      num_episodes: Optional[int]=None,
+                      port: int=DEFAULT_RPC_PORT) -> Dict[str, float]:
+    benchmark = Benchmark(config_paths, eval_remote=False)
+    agent = AgentProxy(f"http://localhost:{port}")
     metrics = benchmark.evaluate(agent, num_episodes)
     agent.terminate()
     return metrics
