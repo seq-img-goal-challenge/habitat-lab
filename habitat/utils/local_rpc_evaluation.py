0c641c7bd9011a81007c1116040879335f00c136
Guillaume Bono <guillaume.bono@insa-lyon.fr>
2021-09-10 11:52:59 +0200

    Added connection retry loop in rpc evaluation

--- habitat/utils/local_rpc_evaluation.py
+++ habitat/utils/local_rpc_evaluation.py
@@ -1,6 +1,7 @@
 from typing import Optional, Dict, Union, Any
 import io
 import threading
+import time
 import xmlrpc.server
 import xmlrpc.client
 
@@ -29,30 +30,46 @@ def _deserialize_visual_observations(buf: bytes) -> Dict[str, np.ndarray]:
 
 
 class AgentProxy(Agent):
-    def __init__(self, agent_url) -> None:
-        self.proxy = xmlrpc.client.ServerProxy(agent_url)
+    def __init__(self, agent_url: str, num_retries: int=5, retry_delay: float=1.0) -> None:
+        self.proxy = xmlrpc.client.ServerProxy(agent_url, use_builtin_types=True)
+        last_error = None
+        for _ in range(num_retries):
+            try:
+                self.proxy.check_init()
+                break
+            except ConnectionRefusedError as e:
+                last_error = e
+                time.sleep(retry_delay)
+        else:
+            raise last_error
 
-    def reset(self):
+    def reset(self) -> None:
         self.proxy.reset()
 
     def act(self, observations: Observations) -> Union[int, str, Dict[str, Any]]:
         serialized_obs = _serialize_visual_observations(observations)
         return self.proxy.act(serialized_obs)
 
-    def terminate(self):
+    def terminate(self) -> None:
         self.proxy.terminate()
 
 
-def run_rpc_agent(agent: Agent, port=DEFAULT_RPC_PORT) -> None:
+def run_rpc_agent(agent: Agent, port: int=DEFAULT_RPC_PORT) -> None:
     with xmlrpc.server.SimpleXMLRPCServer(("localhost", port),
-                                          logRequests=False, allow_none=True) as rpc_server:
+                                          logRequests=False,
+                                          allow_none=True,
+                                          use_builtin_types=True) as rpc_server:
+        @rpc_server.register_function
+        def check_init() -> bool:
+            return True
+
         @rpc_server.register_function
         def reset() -> None:
             agent.reset()
 
         @rpc_server.register_function
-        def act(serialized_obs: xmlrpc.client.Binary) -> Union[int, str, Dict[str, Any]]:
-            obs = _deserialize_visual_observations(serialized_obs.data)
+        def act(serialized_obs: bytes) -> Union[int, str, Dict[str, Any]]:
+            obs = _deserialize_visual_observations(serialized_obs)
             return agent.act(obs)
 
         @rpc_server.register_function
@@ -65,9 +82,10 @@ def run_rpc_agent(agent: Agent, port=DEFAULT_RPC_PORT) -> None:
 
 def run_rpc_benchmark(config_paths: Optional[str]=None,
                       num_episodes: Optional[int]=None,
-                      port: int=DEFAULT_RPC_PORT) -> Dict[str, float]:
+                      port: int=DEFAULT_RPC_PORT,
+                      num_retries: int=5, retry_delay: float=1.0) -> Dict[str, float]:
     benchmark = Benchmark(config_paths, eval_remote=False)
-    agent = AgentProxy(f"http://localhost:{port}")
+    agent = AgentProxy(f"http://localhost:{port}", num_retries, retry_delay)
     metrics = benchmark.evaluate(agent, num_episodes)
     agent.terminate()
     return metrics
