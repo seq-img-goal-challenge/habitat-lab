a8adede6982ef63a08c77be9d5e0ed365bb26972
Guillaume Bono <guillaume.bono@insa-lyon.fr>
2021-09-15 11:07:14 +0200

    Slightly refactored local rpc evaluation to split server (no dep on habitat) and client

--- scripts/seq_objnav_rpc_eval/run_agent.py
+++ scripts/seq_objnav_rpc_eval/run_agent.py
@@ -1,46 +1,10 @@
 import argparse
-import threading
-import io
-import xmlrpc.server
 
 import numpy as np
 import gym
 
+from habitat.utils.rpc_evaluation_server import run_rpc_agent
 from SUBMISSION import SubmittedAgent
-
-
-DEFAULT_RPC_PORT = 8000
-
-
-def _deserialize_visual_observations(buf):
-    buf = io.BytesIO(buf)
-    return {uuid: obs for uuid, obs in np.load(buf).items()}
-
-
-def run_rpc_agent(agent, port=DEFAULT_RPC_PORT):
-    with xmlrpc.server.SimpleXMLRPCServer(("localhost", port),
-                                          logRequests=False,
-                                          allow_none=True,
-                                          use_builtin_types=True) as rpc_server:
-        @rpc_server.register_function
-        def check_init():
-            return True
-
-        @rpc_server.register_function
-        def reset():
-            agent.reset()
-
-        @rpc_server.register_function
-        def act(serialized_obs):
-            obs = _deserialize_visual_observations(serialized_obs)
-            return agent.act(obs)
-
-        @rpc_server.register_function
-        def terminate():
-            t = threading.Thread(target=rpc_server.shutdown)
-            t.start()
-
-        rpc_server.serve_forever()
 
 
 def parse_args():
