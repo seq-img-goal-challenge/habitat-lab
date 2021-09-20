80328da67551ae8d5ffc2206075aa05d688ca3d7
elombardi <nomail>
2021-09-20 12:39:08 +0200

    Prepare merge with commit 141f2236

--- scripts/seq_objnav_rpc_eval/run_benchmark.py
+++ scripts/seq_objnav_rpc_eval/run_benchmark.py
@@ -7,8 +7,10 @@ from habitat.utils.local_rpc_evaluation import run_rpc_benchmark, DEFAULT_RPC_PO
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config-path", "-c", default="configs/tasks/pointnav.yaml")
-    parser.add_argument("--num-episodes", "-n", default=10)
+    parser.add_argument("--num-episodes", "-n", type=int, default=10)
     parser.add_argument("--port", "-p", type=int, default=DEFAULT_RPC_PORT)
+    parser.add_argument("--num-retries", "-r", type=int, default=5)
+    parser.add_argument("--retry-delay", "-d", type=float, default=1.0)
     return parser.parse_args()
 
 
