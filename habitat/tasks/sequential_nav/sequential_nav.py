683c799ec2a0feda87deb362faaecc98447b1729
Guillaume Bono <guillaume.bono@insa-lyon.fr>
2021-09-28 15:12:20 +0200

    Added oracle topdown map sensors for seq nav task

--- habitat/tasks/sequential_nav/sequential_nav.py
+++ habitat/tasks/sequential_nav/sequential_nav.py
@@ -2,7 +2,8 @@ from typing import Any, Optional, List, Dict, ClassVar
 
 import attr
 import numpy as np
-from gym import Space, spaces
+import cv2
+import gym
 
 from habitat.config import Config
 from habitat.core.dataset import Dataset, Episode
@@ -14,7 +15,7 @@ from habitat.tasks.nav.nav import NavigationGoal, NavigationTask, NavigationEpis
                                   PointGoalSensor, IntegratedPointGoalGPSAndCompassSensor, \
                                   TopDownMap
 from habitat.tasks.sequential_nav.utils import make_sequential
-from habitat.utils.visualizations import maps
+from habitat.utils.visualizations import maps, fog_of_war
 
 
 @attr.s(auto_attribs=True, kw_only=True)
@@ -83,6 +84,139 @@ class FoundAction(SimulatorTaskAction):
 SequentialPointGoalSensor = make_sequential(PointGoalSensor)
 SequentialOnlinePointGoalSensor = make_sequential(IntegratedPointGoalGPSAndCompassSensor,
                                                   name="SequentialOnlinePointGoalSensor")
+
+@registry.register_sensor
+class SequentialMapSensor(Sensor):
+    _sim: Simulator
+    _last_ep_id: Optional[str]
+    _topdown_map: Optional[np.ndarray]
+    _fog: Optional[np.ndarray]
+    _origin: Optional[np.ndarray]
+    _mppx: Optional[float]
+
+    def __init__(self, sim: Simulator, config: Config, *args: Any, **kwargs: Any) -> None:
+        self._sim = sim
+        self._last_ep_id = None
+        self._topdown_map = None
+        self._fog = None
+        self._origin = None
+        self._mppx = None
+        super().__init__(*args, sim=sim, config=config, **kwargs)
+
+    def _get_uuid(self, *args: Any, **kwargs: Any) -> str:
+        return "map"
+
+    def _get_sensor_type(self, *args: Any, **kwargs: Any) -> SensorTypes:
+        return SensorTypes.PATH
+
+    def _get_observation_space(self, *args: Any, **kwargs: Any) -> gym.spaces.Space:
+        res = self.config.RESOLUTION
+        return gym.spaces.Box(0, 255, (res, res), np.uint8)
+
+    def get_observation(self, episode: SequentialEpisode,
+                              *args: Any, **kwargs: Any) -> np.ndarray:
+        mrk = self.config.MARKER_SIZE // 2
+        if self._last_ep_id != episode.episode_id:
+            res = self.config.RESOLUTION
+            self._origin, upper = self._sim.pathfinder.get_bounds()
+            mppx = (upper - self._origin) / res
+            self._mppx = max(mppx[0], mppx[2])
+            altitude = self._sim.get_agent_state().position[1]
+            nav_mask = self._sim.pathfinder.get_topdown_view(self._mppx, altitude)
+            h, w = nav_mask.shape
+            self._topdown_map = np.full((res, res), maps.MAP_INVALID_POINT, dtype=np.uint8)
+            oi, oj = (res - h) // 2, (res - w) // 2
+            self._topdown_map[oi:oi + h, oj:oj + w][nav_mask] = maps.MAP_VALID_POINT
+            self._origin[0] -= oj * self._mppx
+            self._origin[1] = altitude
+            self._origin[2] -= oi * self._mppx
+            for step in episode.steps:
+                for goal in step.goals:
+                    j, _, i = ((goal.position - self._origin) / self._mppx).astype(np.int64)
+                    cv2.circle(self._topdown_map, (j, i), mrk,
+                               maps.MAP_TARGET_POINT_INDICATOR, -1)
+            self._fog = np.zeros_like(self._topdown_map)
+            self._last_ep_id = episode.episode_id
+
+        s = self._sim.get_agent_state()
+        j, _, i = ((s.position - self._origin) / self._mppx).astype(np.int64)
+        a = np.pi + 2 * np.arctan(s.rotation.y / s.rotation.w)
+        topdown_map = self._topdown_map.copy()
+        if self.config.FOG_OF_WAR:
+            fov = self.config.HFOV
+            d = self.config.VISIBILITY
+            self._fog = fog_of_war.reveal_fog_of_war(self._topdown_map, self._fog,
+                                                     np.array((i, j)), a, fov, d / self._mppx)
+            topdown_map[self._fog == 0] = maps.MAP_INVALID_POINT
+        cv2.circle(topdown_map, (j, i), mrk, maps.MAP_SOURCE_POINT_INDICATOR, -1)
+        return topdown_map
+
+
+@registry.register_sensor
+class SequentialEgoMapSensor(Sensor):
+    _sim: Simulator
+    _last_ep_id: Optional[str]
+    _topdown_map: Optional[np.ndarray]
+    _fog: Optional[np.ndarray]
+
+    def __init__(self, sim: Simulator, config: Config, *args: Any, **kwargs: Any) -> None:
+        self._sim = sim
+        self._last_ep_id = None
+        self._topdown_map = None
+        self._fog = None
+        super().__init__(*args, sim=sim, config=config, **kwargs)
+
+    def _get_uuid(self, *args: Any, **kwargs: Any) -> str:
+        return "ego_map"
+
+    def _get_sensor_type(self, *args: Any, **kwargs: Any) -> SensorTypes:
+        return SensorTypes.PATH
+
+    def _get_observation_space(self, *args: Any, **kwargs: Any) -> gym.spaces.Space:
+        res = int(2 * self.config.VISIBILITY / self.config.METERS_PER_PIXEL)
+        return gym.spaces.Box(0, 255, (res, res), np.uint8)
+
+    def get_observation(self, episode: SequentialEpisode,
+                              *args: Any, **kwargs: Any) -> np.ndarray:
+        mrk = self.config.MARKER_SIZE // 2
+        mppx = self.config.METERS_PER_PIXEL
+        if self._last_ep_id != episode.episode_id:
+            self._topdown_map = maps.get_topdown_map_from_sim(self._sim, meters_per_pixel=mppx)
+            for step in episode.steps:
+                for goal in step.goals:
+                    i, j = maps.to_grid(goal.position[2], goal.position[0],
+                                        self._topdown_map.shape, self._sim)
+                    cv2.circle(self._topdown_map, (j, i), mrk,
+                               maps.MAP_TARGET_POINT_INDICATOR, -1)
+            self._fog = np.zeros_like(self._topdown_map)
+            self._last_ep_id = episode.episode_id
+
+        s = self._sim.get_agent_state()
+        i, j = maps.to_grid(s.position[2], s.position[0], self._topdown_map.shape, self._sim)
+        a = 2 * np.arctan(s.rotation.y / s.rotation.w)
+        d = self.config.VISIBILITY
+        topdown_map = self._topdown_map.copy()
+        if self.config.FOG_OF_WAR:
+            fov = self.config.HFOV
+            self._fog = fog_of_war.reveal_fog_of_war(self._topdown_map, self._fog,
+                                                     np.array((i, j)), np.pi + a,
+                                                     fov, d / mppx)
+            topdown_map[self._fog == 0] = maps.MAP_INVALID_POINT
+
+        h, w = topdown_map.shape
+        rot = cv2.getRotationMatrix2D((j, i), -np.degrees(a), 1.0)
+        rot_h = int(h * abs(rot[0, 0]) + w * abs(rot[0, 1]))
+        rot_w = int(h * abs(rot[0, 1]) + w * abs(rot[0, 0]))
+        rot[0, 2] += 0.5 * rot_w - j
+        rot[1, 2] += 0.5 * rot_h - i
+        rot_map = cv2.warpAffine(topdown_map, rot, (rot_w, rot_h),
+                                 borderValue=maps.MAP_INVALID_POINT)
+        res = int(2 * d / mppx)
+        ego_map = np.zeros((res, res), dtype=np.uint8)
+        oi, oj = max(0, (res - rot_h) // 2), max(0, (res - rot_w) // 2)
+        y, x = max(0, (rot_h - res) // 2), max(0, (rot_w - res) // 2)
+        ego_map[oi:oi + rot_h, oj: oj + rot_w] = rot_map[y:y + res, x: x + res]
+        return ego_map
 
 
 @registry.register_measure
